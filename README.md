525311133ddec2ffa300a21da7f2160efa80d0dd
jd_7ea3439d92250 <494090597@qq.com>
2014-12-29 19:50:29 +0800

    c9

--- README.md
+++ README.md
@@ -1,14 +1,37 @@
-这是一个示例 project
-使用时，修改 wsgi.py 中的 PROJECT_NAME
 
-示例网站
-http://ziqiangxuetang.jd-app.com/
-后台：http://ziqiangxuetang.jd-app.com/admin/
-后台用户名：twz915
-密码：199110050
-
+     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
+    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
+    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
+    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
+     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
+    ----------------------------------------------------------------- 
 
 
+Welcome to your Django project on Cloud9 IDE!
 
-twz915 [涂伟忠](http://www.tuweizhong.com) 提供
+Your Django project is already fully setup. Just click the "Run" button to start
+the application. On first run you will be asked to create an admin user. You can
+access your application from 'https://blog-nightfarmer.c9.io/' and the admin page from 
+'https://blog-nightfarmer.c9.io/admin'.
 
+## Starting from the Terminal
+
+In case you want to run your Django application from the terminal just run:
+
+1) Run syncdb command to sync models to database and create Django's default superuser and auth system
+
+    $ python manage.py syncdb
+
+2) Run Django
+
+    $ python manage.py runserver $IP:$PORT
+    
+## Support & Documentation
+
+Django docs can be found at https://www.djangoproject.com/
+
+You may also want to follow the Django tutorial to create your first application:
+https://docs.djangoproject.com/en/1.7/intro/tutorial01/
+
+Visit http://docs.c9.io for support, or to learn more about using Cloud9 IDE.
+To watch some training videos, visit http://www.youtube.com/user/c9ide
--- blog/settings.py
+++ blog/settings.py
@@ -2,10 +2,10 @@
 Django settings for blog project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+https://docs.djangoproject.com/en/1.7/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -14,17 +14,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$nq0vc87=)zykd18ggru8hila)p2j7x17p)i&=441la0rn^qtj'
+SECRET_KEY = 'dak(j7l-4$j#5d3eld-_+tp%$_f8m($hex)kv2!qac67eftelx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Application definition
@@ -43,6 +44,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -53,7 +55,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -63,7 +65,7 @@ DATABASES = {
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -77,6 +79,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
